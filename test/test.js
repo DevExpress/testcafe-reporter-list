00416a1ff05d6401f8d7e9cc61a555b02d60ece4
inikulin <ifaaan@gmail.com>
2015-12-04 17:30:04 +0300

    Fix test file path

--- test/test.js
+++ test/test.js
@@ -44,7 +44,7 @@ function createReport (withColors) {
 
 it('Should produce report with colors', function () {
     var report   = createReport(true);
-    var expected = JSON.parse(read('./data/report-with-colors'));
+    var expected = JSON.parse(read('./data/report-with-colors.json'));
 
     report   = normalizeNewline(report).trim();
     expected = normalizeNewline(expected).trim();
