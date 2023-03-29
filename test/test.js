00aa8b2c911fe31be355c2264eca4f2e0b5f6185
artem-babich <artem.babich@devexpress.com>
2023-03-29 20:58:14 +0400

    remove debug methods

--- test/test.js
+++ test/test.js
@@ -3,14 +3,11 @@ const normalizeNewline = require('normalize-newline');
 const read             = require('read-file-relative').readSync;
 const OS               = require('os-family');
 const createReport     = require('./utils/create-report');
-const fs               = require('fs');
 
 it('Should produce report with colors', function () {
     const expectedFile = OS.win ? './data/report-with-colors-win.json' : './data/report-with-colors.json';
-    let report       = createReport(true);
-    let expected     = JSON.parse(read(expectedFile));
-
-    fs.writeFileSync('report-with-colors.json', JSON.stringify(report));
+    let report         = createReport(true);
+    let expected       = JSON.parse(read(expectedFile));
 
     report   = normalizeNewline(report).trim();
     expected = normalizeNewline(expected).trim();
@@ -21,8 +18,6 @@ it('Should produce report with colors', function () {
 it('Should produce report without colors', function () {
     let report   = createReport(false);
     let expected = read('./data/report-without-colors');
-
-    fs.writeFileSync('report-without-colors', report);
 
     report   = normalizeNewline(report).trim();
     expected = normalizeNewline(expected).trim();
